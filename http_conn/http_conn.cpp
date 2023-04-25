28478fbed6ae4473b0f55373afde8be582023f2b
22125236 <13633217285@163.com>
2023-04-25 07:37:51 -0700

    add http parse

--- http_conn/http_conn.cpp
+++ http_conn/http_conn.cpp
@@ -1,7 +1,19 @@
 #include "http_conn.h"  
 
+// 定义HTTP响应的一些状态信息
+const char* ok_200_title = "OK";
+const char* error_400_title = "Bad Request";
+const char* error_400_form = "Your request has bad syntax or is inherently impossible to satisfy.\n";
+const char* error_403_title = "Forbidden";
+const char* error_403_form = "You do not have permission to get file from this server.\n";
+const char* error_404_title = "Not Found";
+const char* error_404_form = "The requested file was not found on this server.\n";
+const char* error_500_title = "Internal Error";
+const char* error_500_form = "There was an unusual problem serving the requested file.\n";
+
 int http_conn::m_epollfd = -1;
 int http_conn::m_user_count = 0;
+const char * doc_root = "/home/wwy/resource";
 
 void setnonblocking(int fd)
 {
@@ -218,12 +230,53 @@ http_conn::HTTP_CODE http_conn::prase_request_line(char * text)
 
 http_conn::HTTP_CODE http_conn::prase_request_header(char * text)
 {
-
+    if (text[0] == '\0')
+    {
+        if (m_content_length != 0)
+        {
+            m_check_state = CHECK_STATE_CONTENT;
+            return NO_REQUEST;
+        }
+        return GET_REQUEST;
+    }
+    else if (strncasecmp(text, "Connection:", 11) == 0) 
+    {
+        // 处理Connection头部字段 Connection: keep=alive
+        text += 11; // 找一下一组命令
+        text += strspn(text, " \t");
+        if (strcasecmp(text , "keep=alive") == 0) {
+            m_linger = true; // 如果设置为keep=alive那么就是保持连接
+        }
+    }
+    else if (strncasecmp(text, "Content-Length:", 15) == 0)
+    {
+        // 处理Content-Length头部字段
+        text += 15;
+        text += strspn(text, " \t"); // 通通都越过找到内容对应索引
+        m_content_length = atol(text);
+    }
+    else if (strncasecmp(text, "Host:", 5) == 0) 
+    {
+        // 处理host头部字段
+        text += 5;
+        text += strspn(text, " \t");
+        m_host = text;
+    }
+    else
+    {
+        // 留作其他解析
+    }
+    return NO_REQUEST;
 }
 
 http_conn::HTTP_CODE http_conn::prase_request_body(char * text)
 {
-
+    if (m_read_idx >= (m_content_length + m_checked_index))
+    {
+        text[m_content_length] = '\0';
+        return GET_REQUEST;
+    }
+    return NO_REQUEST;
 }
 
 // 获取一行数据，判断依据 \r\n
@@ -265,15 +318,152 @@ http_conn::LINE_STATUS http_conn::parse_line()
     return LINE_OK;
 }
 
+// 当得到一个完整、正确的HTTP请求时，我们就分析目标文件的属性
+// 如果目标文件存在、对所有用户可读，且不是目录，则使用mmap将其
+// 映射到内存地址m_file_address处，并告诉调用者获取文件成功
 http_conn::HTTP_CODE http_conn::do_request()
 {
+    strcpy(m_real_file, doc_root);
+    int len = strlen(doc_root);
+    strncpy(m_real_file + len, m_url, FILENAME_LEN-len-1);
+    // stat 获取文件状态信息，-1失败，0成功
+    if (stat(m_real_file, &m_file_stat) < 0)
+    {
+        return NO_RESOURCE;
+    }
+    // 判断访问权限
+    if (! (m_file_stat.st_mode & S_IROTH))
+    {
+        return FORBIDDEN_REQUEST;
+    }
+    // 判断是否是目录
+    if (S_ISDIR(m_file_stat.st_mode))
+    {
+        return BAD_REQUEST;
+    }
+    // 只读方式打开
+    int fd = open(m_real_file, O_RDONLY);
+    m_file_address = (char* ) mmap(0, m_file_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    close(fd);
+    return FILE_REQUEST;
+}
 
+void http_conn::unmap()
+{
+    if (m_file_address)
+    {
+        munmap(m_file_address, m_file_stat.st_size);
+        m_file_address = 0;
+    }
+}
+
+// 往写缓冲中写入待发送的数据
+bool http_conn::add_response(const char* format, ... ) {
+    if(m_write_idx >= WRITE_BUFFER_SIZE) { // 如果当前的写索引大于缓冲区大小
+        return false;
+    }
+    va_list arg_list; // 解析多参数 
+    va_start( arg_list, format );
+    int len = vsnprintf(m_write_buff + m_write_idx, WRITE_BUFFER_SIZE - 1 - m_write_idx, format, arg_list );
+    if( len >= (WRITE_BUFFER_SIZE - 1 - m_write_idx)) {
+        return false;
+    }
+    m_write_idx += len;
+    va_end(arg_list);
+    return true;
+}
+
+bool http_conn::add_status_line(int status, const char* title)
+{
+    return add_response("%s %d %s\r\n", "HTTP/1.1", status, title);
+}   
+
+void http_conn::add_headers(int content_len)
+{
+    add_content_length(content_len);
+    add_content_type();
+    add_linger();
+    add_blank_line();
+}
+
+bool http_conn::add_content_length(int content_len)
+{
+    return add_response("Content-Length: %d\r\n", content_len);
+}
+
+bool http_conn::add_content_type()
+{
+    return add_response("Content-Type:%s\r\n", "text/html");
+}
+
+bool http_conn::add_linger()
+{
+    return add_response("Connection: %s\r\n", (m_linger == true) ? "keep-alive" : "close");
+}
+
+bool http_conn::add_blank_line()
+{
+    return add_response("%s", "\r\n");
+}
+
+bool http_conn::add_content(const char* content)
+{
+    return add_response("%s", content);
 }
 
 bool http_conn::write()
 {
-    printf("write\n");
-    return true;
+    int temp = 0;
+}
+
+bool http_conn::process_write(HTTP_CODE ret)
+{
+    switch(ret)
+    {
+        case INTERNAL_ERROR:
+            add_status_line(500, error_500_title);
+            add_headers(strlen(error_500_form));
+            if (!add_content(error_500_form))
+            {
+                return false;
+            }
+            break;
+        case BAD_REQUEST:
+            add_status_line(400, error_400_title);
+            add_headers(strlen(error_400_form));
+            if (!add_content(error_400_form))
+            {
+                return false;
+            }
+            break;
+        case NO_RESOURCE:
+            add_status_line(404, error_404_title);
+            add_headers(strlen(error_404_form));
+            if (!add_content(error_404_form))
+            {
+                return false;
+            }
+            break;
+        case FORBIDDEN_REQUEST:
+            add_status_line(403, error_403_title);
+            add_headers(strlen(error_403_form));
+            if (!add_content(error_400_form))
+            {
+                return false;
+            }
+            break;
+        case FILE_REQUEST:
+            add_status_line(200, ok_200_title);
+            add_headers(m_file_stat.st_size);
+            m_iv[ 0 ].iov_base = m_write_buff; // [状态行 和 响应头部]存储在写内存缓冲区的起始地址
+            m_iv[ 0 ].iov_len = m_write_idx;  // 这个是内存缓冲区的长度
+            m_iv[ 1 ].iov_base = m_file_address; // 这个是文件内容的起始地址
+            m_iv[ 1 ].iov_len = m_file_stat.st_size; // 这个是文件内容的长度
+            m_iv_count = 2; // m_iv_count表示被写内存块的数量
+            return true;
+        default:
+            return false;
+    }
 }
 
 // 由线程池中的工作线程调用，是处理HTTP请求的入口函数
@@ -288,4 +478,10 @@ void http_conn::process()
     }
 
     // 生成响应
+    bool write_ret = process_write(read_ret); 
+    if (!write_ret)
+    {
+        close_conn();
+    }
+    modifyfd(m_epollfd, m_sockfd, EPOLLOUT);
 }
